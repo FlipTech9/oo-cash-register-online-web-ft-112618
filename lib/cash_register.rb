045351a08f56a4d5ba61389ecb92a4b0ab4a94d5
Alex S <alex.sunga@fliptech.co>
2018-12-08 00:22:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
-
+require 'pry'
+class CashRegister
+  attr_accessor :total
+  attr_reader :discount
+  
+  def initialize (discount = 0)
+    @total = 0
+    # binding.pry
+    @discount = discount
+  end
+  
+  def add_item (item, price, quantity = 1)
+    @total = @total + price * quantity
+  end 
+  
+  def apply_discount
+    # binding.pry
+    self.total = self.total * (1 - self.discount.to_f / 100)
+    puts 
+  end
+end
